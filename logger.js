cbfb583cdafd645a440628526d969deef269477f
Henrique Moraes <henriquemsm@outlook.com>
2021-03-29 20:39:10 -0300

    Estudo sobre API'S

--- logger.js
+++ logger.js
@@ -1,1 +1,16 @@
 // Arquivo que ira armazenar mensagens do sistema
+const events = require("events");
+
+// Criando um evento, como o addEventList que vai disparar uma mensagem quando o usuario digitar hi no console.
+const sayHiEvent = new events.EventEmitter();
+
+sayHiEvent.on("hi", () => console.log("Coe ze !!"));
+const log = function (message) {
+  sayHiEvent.emit(message);
+};
+
+module.exports = "log";
+
+// document
+//   .querySelector("button")
+//   .addEventListener("click", (e) => console.log(e));
--- stats.js
+++ stats.js
@@ -1,7 +1,11 @@
 // P/ executar o node utiliza-se node stats.js
 
+// API : Application Programming Interface: É um pedaço de progrma que pode ser usado por outro pedaço de programa, em ordem de basicamente permitir que aplicações se comunicarem entre si e trocam informações (dados).
+// HTTP REQUEST: Client faz uma requisição de maneira assincrona, ou seja, solicita (get) ou envia (post) algum dado a um web server que normalmente contem uma API. O web server ira retornar uma resposta contendo o dado solicitado por meio
+
 // Require -> Importando modulos no Node
 const os = require("os");
+const logger = require("log");
 
 setInterval(() => {
   const { freemem, totalmem } = os;
@@ -20,3 +24,7 @@ setInterval(() => {
   console.log("======= PC STATUS =======");
   console.table(stats);
 }, 1000);
+
+window.prompt();
+logger(window.prompt("Bem vindo usuario !!"));
+console.log("Teste");
